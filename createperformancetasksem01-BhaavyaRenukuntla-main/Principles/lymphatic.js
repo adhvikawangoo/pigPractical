58ce3a59f93801e57d4b6481ecfb445db8a04090
BhaavyaRenukuntla <brenukuntla476@student.fuhsd.org>
2024-12-16 08:43:50 -0800

    Add files via upload

---
+++ createperformancetasksem01-BhaavyaRenukuntla-main/Principles/lymphatic.js
@@ -0,0 +1,205 @@
+// script.js
+
+let questions = [
+    {
+        prompt: `The lymphatic system consists of: `,
+        options: [
+            "lymphatic vessels and lymphoid organs",
+            "all of the plasma component of the bloodstream",
+            "all fluids inside the body's cells",
+            "all liquids in the body whether inside cells or in spaces between tissues",
+        ],
+        answer: "lymphatic vessels and lymphoid organs",
+    },
+
+    {
+        prompt: `Lymph is prevented from flowing backwards by: `,
+        options: [
+            "blood pressure",
+            "suction from contracting lymph nodes",
+            "one-way valves",
+            "cilia lining the lymphatic system",
+        ],
+        answer: "Diaphragm",
+    },
+
+    {
+        prompt: `The subclavian veins are cardiovascular veins in the abdomen.`,
+        options: [
+            "true",
+            "false",
+        ],
+        answer: "true",
+    },
+
+    {
+        prompt: `Lymph flows from lymphatic capillaries into ever-larger lymphatic vessels and finally to a lymphatic duct.`,
+        options: [
+            "true",
+            "false",
+        ],
+        answer: "true",
+    },
+
+    {
+        prompt: `The tonsils and adenoids (pharyngeal tonsils) are composed of partly encapsulated lymph nodules.`,
+        options: [
+            "true",
+            "false",
+        ],
+        answer: "true",
+    },
+];
+
+// Get Elements
+//Cited: GeeksForGeeks.org
+let questionsEl =
+    document.querySelector(
+        "#questions"
+    );
+let timerEl =
+    document.querySelector("#timer");
+let choicesEl =
+    document.querySelector("#options");
+let submitBtn = document.querySelector(
+    "#submit-score"
+);
+let startBtn =
+    document.querySelector("#start");
+let nameEl =
+    document.querySelector("#name");
+let feedbackEl = document.querySelector(
+    "#feedback"
+);
+let reStartBtn =
+    document.querySelector("#restart");
+
+//amount of time per question is 10, multiplied by number of questions
+let currentQuestionIndex = 0;
+let time = questions.length * 10;
+let timerId;
+
+// Function to randomize the questions array 
+function randomizeQuestions(questionsArray) 
+{
+	if(questionsArray.length < 1)
+	{
+		return "no questions for this topic";
+	}
+	else
+	{
+		for (let i = questionsArray.length - 1; i > 0; i--) 
+		{
+			// Generate a random index from 0 to i
+			let randomIndex = Math.floor(Math.random() * (i + 1));
+			
+			// Swap the current element with the random element
+			let temp = questionsArray[i];
+			questionsArray[i] = questionsArray[randomIndex];
+			questionsArray[randomIndex] = temp;
+		}
+	}
+}
+
+// Start quiz and hide frontpage
+function quizStart() {
+    // Randomize the questions before starting the quiz
+    randomizeQuestions(questions);
+
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    let landingScreenEl = document.getElementById("start-screen");
+    landingScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    getQuestion();
+}
+
+// Loop through array of questions and answers and create list with buttons
+function getQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let promptEl = document.getElementById("question-words");
+    promptEl.textContent = currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(function (choice, i) {
+        let choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.onclick = questionClick;
+        choicesEl.appendChild(choiceBtn);
+    });
+}
+
+// Check for right answers and deduct time for wrong answer, go to next question
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = `Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
+        feedbackEl.style.color = "red";
+    } else {
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.style.color = "green";
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 2000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+// End quiz by hiding questions, Stop timer and show final score
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl = document.getElementById("quiz-end");
+    endScreenEl.removeAttribute("class");
+    let finalScoreEl = document.getElementById("score-final");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+}
+
+// End quiz if timer reaches 0
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+// Save score in local storage along with users' name
+function saveHighscore() {
+    let name = nameEl.value.trim();
+    if (name !== "") {
+        let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+        let newScore = {
+            score: time,
+            name: name,
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+        alert("Your Score has been Submitted");
+    }
+}
+
+// Save users' score after pressing enter
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+        alert("Your Score has been Submitted");
+    }
+}
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+startBtn.onclick = quizStart;
---
+++ createperformancetasksem01-BhaavyaRenukuntla-main/Principles/script.js
@@ -0,0 +1,265 @@
+// script.js
+
+let questions = [
+    {
+        prompt: `How does food move through your digestive tract?`,
+        options: [
+            "By gravity",
+            "Peristalsis",
+            "Cilia",
+            "Chemical Absorption",
+        ],
+        answer: "Peristalsis",
+    },
+
+    {
+        prompt: `Where does most of the digestive process take place?`,
+        options: [
+            "Small intestine",
+            "Large intestine",
+            "Stomach",
+            "All of the above",
+        ],
+        answer: "All of the above",
+    },
+
+    {
+        prompt: `What does the liver do to help digestion?`,
+        options: [
+            "Makes important enzymes",
+            "Neutralizes stomach acid",
+            "Produces bile",
+            "Regulates insulin",
+        ],
+        answer: "Produces bile",
+    },
+
+    {
+        prompt: `Which of these can cause heartburn?`,
+        options: [
+            "Being overweight",
+            "Lying down soon after eating a large meal",
+            "Eating high-fat foods",
+            "All of the above",
+        ],
+        answer: "All of the above",
+    },
+
+    {
+        prompt: `Which of these causes ulcers?`,
+        options: [
+            "Bacterial infection in the stomach",
+            "Aspirin and other anti-inflammatory medicines",
+            "Chronic stress",
+            "A and B",
+        ],
+        answer: "A and B",
+    },
+];
+
+// Get Elements
+//Cited: GeeksForGeeks.org
+let questionsEl =
+    document.querySelector(
+        "#questions"
+    );
+let timerEl =
+    document.querySelector("#timer");
+let choicesEl =
+    document.querySelector("#options");
+let submitBtn = document.querySelector(
+    "#submit-score"
+);
+let startBtn =
+    document.querySelector("#start");
+let nameEl =
+    document.querySelector("#name");
+let feedbackEl = document.querySelector(
+    "#feedback"
+);
+let reStartBtn =
+    document.querySelector("#restart");
+
+// Quiz's initial state
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
+
+// Start quiz and hide frontpage
+
+function quizStart() {
+    timerId = setInterval(
+        clockTick,
+        1000
+    );
+    timerEl.textContent = time;
+    let landingScreenEl =
+        document.getElementById(
+            "start-screen"
+        );
+    landingScreenEl.setAttribute(
+        "class",
+        "hide"
+    );
+    questionsEl.removeAttribute(
+        "class"
+    );
+    getQuestion();
+}
+
+// Loop through array of questions and
+// Answers and create list with buttons
+function getQuestion() {
+    let currentQuestion =
+        questions[currentQuestionIndex];
+    let promptEl =
+        document.getElementById(
+            "question-words"
+        );
+    promptEl.textContent =
+        currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(
+        function (choice, i) {
+            let choiceBtn =
+                document.createElement(
+                    "button"
+                );
+            choiceBtn.setAttribute(
+                "value",
+                choice
+            );
+            choiceBtn.textContent =
+                i + 1 + ". " + choice;
+            choiceBtn.onclick =
+                questionClick;
+            choicesEl.appendChild(
+                choiceBtn
+            );
+        }
+    );
+}
+
+// Check for right answers and deduct
+// Time for wrong answer, go to next question
+
+function questionClick() {
+    if (
+        this.value !==
+        questions[currentQuestionIndex]
+            .answer
+    ) {
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = `Wrong! The correct answer was 
+        ${questions[currentQuestionIndex].answer}.`;
+        feedbackEl.style.color = "red";
+    } else {
+        feedbackEl.textContent =
+            "Correct!";
+        feedbackEl.style.color =
+            "green";
+    }
+    feedbackEl.setAttribute(
+        "class",
+        "feedback"
+    );
+    setTimeout(function () {
+        feedbackEl.setAttribute(
+            "class",
+            "feedback hide"
+        );
+    }, 2000);
+    currentQuestionIndex++;
+    if (
+        currentQuestionIndex ===
+        questions.length
+    ) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+// End quiz by hiding questions,
+// Stop timer and show final score
+
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl =
+        document.getElementById(
+            "quiz-end"
+        );
+    endScreenEl.removeAttribute(
+        "class"
+    );
+    let finalScoreEl =
+        document.getElementById(
+            "score-final"
+        );
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute(
+        "class",
+        "hide"
+    );
+}
+
+// End quiz if timer reaches 0
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+// Save score in local storage
+// Along with users' name
+
+function saveHighscore() {
+    let name = nameEl.value.trim();
+    if (name !== "") {
+        let highscores =
+            JSON.parse(
+                window.localStorage.getItem(
+                    "highscores"
+                )
+            ) || [];
+        let newScore = {
+            score: time,
+            name: name,
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem(
+            "highscores",
+            JSON.stringify(highscores)
+        );
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+
+// Save users' score after pressing enter
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+
+startBtn.onclick = quizStart;
